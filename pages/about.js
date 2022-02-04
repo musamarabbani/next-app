47077ea1bfe0d317448a8b0284b5820b95d75faa
M Usama Rabbani <musamarabbani@192.168.20.28>
2022-02-04 11:51:55 +0500

    article and articleItems basic concept

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,14 @@
-import Head from 'next/head';
+import Meta from '../components/Meta';
 import styles from '../styles/Layout.module.css';
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
-			<Head>
-				<title>About Page</title>
-				<meta name='description' content='this is user description page' />
-				<meta name='keywords' content='web development, programming' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
+			<Meta
+				title='About Page'
+				description='Visit to About Page'
+				keywords='web apps development, web programming'
+			/>
 			<div>
 				<h1>Welcome to About Page</h1>
 			</div>
--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,9 +1,53 @@
+import React from 'react';
+import axios from 'axios';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Meta from '../../../components/Meta';
 
-const Article = () => {
+const Article = ({ article }) => {
 	const router = useRouter();
 	const { id } = router.query;
-	return <div>This is article id{id}</div>;
+	return (
+		<React.Fragment>
+			<Meta
+				title={article.title}
+				keywords={'get specific article'}
+				description={article.excerpt}
+			/>
+			<h1>{article.title}</h1>
+			<p>{article.body}</p>
+			<br />
+			<Link href='/'>Go Back</Link>
+		</React.Fragment>
+	);
+};
+
+export const getStaticProps = async (context) => {
+	try {
+		const response = await axios.get(
+			`https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+		);
+		if (response.status >= 400) throw new Error(response);
+		return {
+			props: { article: response.data },
+		};
+	} catch (error) {
+		console.log('getServerSideProps error', error);
+	}
+};
+
+export const getStaticPaths = async () => {
+	try {
+		const response = await axios.get(
+			'https://jsonplaceholder.typicode.com/posts'
+		);
+		const articles = response.data;
+		const ids = articles.map((article) => article.id);
+		const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+		return { paths: paths, fallback: false };
+	} catch (error) {
+		console.log('getStaticPath ==>', error);
+	}
 };
 
 export default Article;
